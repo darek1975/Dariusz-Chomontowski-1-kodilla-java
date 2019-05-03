6f45e00d772f51b9da867dfacbad17f66680c732
darek1975 <dchomontowski@wp.pl>
2019-05-03 16:10:05 +0100

    First Commit

--- kodilla-exception/src/main/java/com/kodilla/exception/main/Exception/Module/Runner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/Exception/Module/Runner.java
@@ -1,4 +1,10 @@
 package com.kodilla.exception.main.Exception.Module;
 
+import com.kodilla.exception.io.FileReader;
+
 public class Runner {
+    public static void main(String args[]){
+        FileReader fileReader = new FileReader();
+        fileReader.readFile();
+    }
 }
--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,4 +1,27 @@
 package com.kodilla.exception.test;
 
 public class FirstChallenge {
+
+    public double divide(double a, double b) throws ArithmeticException {
+        if(b == 0){
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+
+
+    public static void main(String[] args) {
+
+        FirstChallenge firstChallenge = new FirstChallenge();
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println("Wynik dzielenia wynosi: "+ result);
+        } catch (ArithmeticException e) {
+            System.out.println("Popełniłeś szkolny błąd: nie można podzielić przez zero: " + e);
+        } finally {
+            System.out.println("Dziękuję za skorzystanie z metody.");
+        }
+    }
 }
+

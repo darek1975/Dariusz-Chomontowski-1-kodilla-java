ad31508587420ecbd309bfe3313871ae8f4f25ca
darek1975 <dchomontowski@wp.pl>
2019-05-26 15:54:28 +0100

    First Commit

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuit.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/StatisticsTestSuit.java
@@ -24,7 +24,7 @@ public class StatisticsTestSuit {
         List<String> testUsers = new ArrayList<String>();
         when(statisticsMock.usersNames()).thenReturn(testUsers);
         when(statisticsMock.postsCount()).thenReturn(0);
-        when(statisticsMock.commentsCount()).thenReturn(120);
+        when(statisticsMock.commentsCount()).thenReturn(0);
 
         ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
 
@@ -33,6 +33,8 @@ public class StatisticsTestSuit {
         forumStatistics.showStatistics();
         //Then
         Assert.assertEquals(0, forumStatistics.averagePostsPerUser, 0.001);
+        Assert.assertEquals(0, forumStatistics.averageCommentsPerUser, 0.001);
+        Assert.assertEquals(0, forumStatistics.averageCommentsPerPost, 0.001);
     }
 
     @Test
@@ -56,6 +58,8 @@ public class StatisticsTestSuit {
         forumStatistics.showStatistics();
         //Then
         Assert.assertEquals(0.2, forumStatistics.averagePostsPerUser, 0.001);
+        Assert.assertEquals(1.2, forumStatistics.averageCommentsPerUser, 0.001);
+        Assert.assertEquals(6, forumStatistics.averageCommentsPerPost, 0.001);
     }
 
     @Test
@@ -79,6 +83,58 @@ public class StatisticsTestSuit {
         forumStatistics.showStatistics();
         //Then
         Assert.assertEquals(2.0, forumStatistics.averagePostsPerUser, 0.001);
+        Assert.assertEquals(1.2, forumStatistics.averageCommentsPerUser, 0.001);
+        Assert.assertEquals(0.6, forumStatistics.averageCommentsPerPost, 0.001);
+
+    }
+    @Test
+    public void testCalculateAdvStatisticsWhenPost10Comments0User10() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        ArrayList<String> testUser = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            testUser.add("user");
+        }
+
+        when(statisticsMock.usersNames()).thenReturn(testUser);
+        when(statisticsMock.postsCount()).thenReturn(10);
+        when(statisticsMock.commentsCount()).thenReturn(0);
+
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+
+        //When
+        forumStatistics.calculateAdvStatistics(statisticsMock);
+        forumStatistics.showStatistics();
+        //Then
+        Assert.assertEquals(1.0, forumStatistics.averagePostsPerUser, 0.001);
+        Assert.assertEquals(0.0, forumStatistics.averageCommentsPerUser, 0.001);
+        Assert.assertEquals(0.0, forumStatistics.averageCommentsPerPost, 0.001);
+
+    }
+    @Test
+    public void testCalculateAdvStatisticsWhenPost1000Comments200User10() {
+        //Given
+        Statistics statisticsMock = mock(Statistics.class);
+
+        ArrayList<String> testUser = new ArrayList<String>();
+        for (int i = 0; i < 10; i++) {
+            testUser.add("user");
+        }
+
+        when(statisticsMock.usersNames()).thenReturn(testUser);
+        when(statisticsMock.postsCount()).thenReturn(1000);
+        when(statisticsMock.commentsCount()).thenReturn(200);
+
+        ForumStatistics forumStatistics = new ForumStatistics(statisticsMock);
+
+        //When
+        forumStatistics.calculateAdvStatistics(statisticsMock);
+        forumStatistics.showStatistics();
+        //Then
+        Assert.assertEquals(100.0, forumStatistics.averagePostsPerUser, 0.001);
+        Assert.assertEquals(20.0, forumStatistics.averageCommentsPerUser, 0.001);
+        Assert.assertEquals(0.2, forumStatistics.averageCommentsPerPost, 0.001);
 
     }
 }

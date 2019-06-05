fb7fa04b382519244e36a14ec3feced5a884c5fb
darek1975 <dchomontowski@wp.pl>
2019-06-05 17:06:21 +0100

    First Commit

---
+++ kodilla-hibernat/src/main/java/com/kodilla/kodillahibernat/KodillaHibernatApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.kodillahibernat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodillaHibernatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernatApplication.class, args);
+    }
+
+}
